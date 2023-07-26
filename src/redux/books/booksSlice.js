ad038fe1cc9aa7902d093e21ccf5bc820575b11f
binyamT <binyampowerhc@gmail.com>
2023-07-26 14:45:13 +0300

    fixed linter errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,22 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    books: []
-}
+  books: [],
+};
 
 const booksSlice = createSlice({
-    name: 'books',
-    initialState,
-    reducers: {
-      addBook: (state, action) => {
-        state.books.push(action.payload);
-      },
-      removeBook: (state, action) => {
-        state.books = state.books.filter((book) => book.item_id !== action.payload);
-      },
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
     },
-  });
-  
-  export default booksSlice.reducer;
-  export const { addBook, removeBook } = booksSlice.actions;
-  
+    removeBook: (state, action) => {
+      state.books = state.books.filter((book) => book.item_id !== action.payload);
+    },
+  },
+});
+
+export default booksSlice.reducer;
+export const { addBook, removeBook } = booksSlice.actions;
--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,20 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    categories: []
-}
+  categories: [],
+};
 
 const categoriesSlice = createSlice({
-    name: 'categories',
-    initialState,
-    reducers: {
-      checkStatus: (state, action) => { 
-        state.categories = action.payload === 'Under construction'
-          ? ['Under construction']
-          : state.categories;
-      },
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? ['Under construction']
+        : state.categories;
     },
-  });
-  
-  export default categoriesSlice.reducer;
-  export const { checkStatus } = categoriesSlice.actions;
+  },
+});
+
+export default categoriesSlice.reducer;
+export const { checkStatus } = categoriesSlice.actions;
